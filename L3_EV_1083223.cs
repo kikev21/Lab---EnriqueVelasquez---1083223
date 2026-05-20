8d4d85866bc7c292468f3d3c629cddb428bcd21f
kikev21 <kikito2154@gmail.com>
2026-05-20 09:08:03 -0600

    Add files via upload

---
+++ L3_EV_1083223.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//Enrique Velasquez
+
+// Espacio de nombres:
+// Se utiliza para organizar el código del programa.
+namespace Laboratorio3
+{
+    // Clase principal del programa
+    class Program
+    {
+        // Método principal:
+        // Punto de inicio de la ejecución del programa.
+        static void Main(string[] args)
+        {
+            //leer la variable nombre
+            Console.WriteLine("Escriba por favor su nombre:");
+            string nombre = Console.ReadLine();
+
+            //Leer la varia de curso o clase
+            Console.WriteLine("Escriba el nombre del curso:");
+            string clase = Console.ReadLine();
+
+            //Mostrar datos
+            Console.WriteLine(clase);
+            Console.WriteLine($"Hola {nombre}, ojalá lo estés pasando genial y que tengas un buen día.");
+            Console.WriteLine("Presione una tecla para salir del programa...");
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
